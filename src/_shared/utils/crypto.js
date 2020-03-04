5ca80c27bf7af535037279628c31f216b72ee9c5
mklemann <matheus.souza@linkapi.com.br>
2020-03-04 09:56:52 -0300

    fix crypto

--- src/_shared/utils/crypto.js
+++ src/_shared/utils/crypto.js
@@ -3,13 +3,13 @@ require('dotenv').config()
 
 module.exports = {
     encrypt(text) {
-        const cipher = crypto.createCipher(process.env.ALGORITHM, process.env.ALGORITHM)
-        let crypted = cipher.update(text, 'utf8', 'hex')
+        const cipher = crypto.createCipher(process.env.ALGORITM, process.env.KEY_CRYPTO)
+        let crypted = cipher.updateH(text, 'utf8', 'hex')
         crypted += cipher.final('hex')
         return crypted
     },
     decrypt(text) {
-        const decipher = crypto.createDecipher(process.env.ALGORITHM, process.env.ALGORITHM)
+        const decipher = crypto.createDecipher(process.env.ALGORITHM, process.env.KEY_CRYPTO)
         let dec = decipher.update(text, 'hex', 'utf8')
         dec += decipher.final('utf8')
         return dec
